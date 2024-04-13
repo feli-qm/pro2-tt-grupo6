2f7cbd199e5125880d37c768e208165d7738b673
feli-qm <fquirogamange@udesa.edu.ar>
2024-04-13 20:00:53 -0300

    eliminar archivos y carp repetidas

--- ‘ti_progra2’/controllers/productControllers.js
+++
@@ -1,7 +0,0 @@
-const data = require('../db/data');
-const productController= {
-    index: function(req, res) {
-        return res.render("productos",{listado: data.productos}); 
-    }};
-
-module.exports= productController;
