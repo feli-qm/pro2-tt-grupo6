7d952ecd6f2db97004d0e3f24d2172c0883f45aa
VickyForginieri <vforginieri@udesa.edu.ar>
2024-04-15 16:24:32 -0300

    modificar controller para mostrar comentario de detalles

--- ti_progra2/controllers/productsController.js
+++ ti_progra2/controllers/productsController.js
@@ -4,7 +4,7 @@ const products = require('../db/products');
 //crear el modulo en si
 const productsController= {
     index: function(req, res) {             //el next va??//
-        return res.render('product'); 
+        return res.render('product' , {productoEncontrado: products.productos[0]}); 
     },
     add: function(req,res) {
         return res.render('product-add');   //sumamos tmb products/indexhtml? (logo)
