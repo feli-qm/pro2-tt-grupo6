13d73f73e676b5a0d23f5d66fdd70bad08ed2f28
feli-qm <fquirogamange@udesa.edu.ar>
2024-04-15 12:22:12 -0300

    headerLogueado y modificaciones en views, rutas y db

--- 'ti_progra2'/db/products.js
+++ 'ti_progra2'/db/products.js
@@ -1,104 +1,105 @@
-const products={
-    usuario : {
+const products = {
+    usuario: {
         email: "juanamancurti@gmail.com", //COINCIDIR CON EL FORMULARIO DE REGISTRO
         usuario: "juamancurti",
-        contraseña: "secreto123",
-        fechaNacimiento: "1990-08-25",
-        numeroDocumento: 98765432,
-        fotoPerfil: "/img/perfiles/beautyqueen.jpg"
+        contrasenia: "secreto123",
+        fechaNacimiento: "1990-08-25",      // esta bien el formato???
+        numeroDocumento: 46213457,
+        fotoPerfil: "https://cdn-icons-png.flaticon.com/512/1361/1361728.png"     //puede ser online o tiene que estar en carpeta public??
     },
-        productos: [
-            {
-                nombre: "Corrector líquido",
-                descripcion: "Corrector líquido de alta cobertura para ocultar imperfecciones y ojeras.",
-                precio: 17.99,
-                imagen: "/img/productos/corrector.jpg",
-                comentarios: [
-                    {
-                        nombreUsuario: "Valen123",
-                        textoComentario: "Este corrector líquido es mi salvavidas. Cubre perfectamente mis ojeras sin verse pesado.",
-                        imagenPerfil: "./public/products/img-cafetera-moulinex.png" //HAY QUE CAMBIAR LA RUTA DE IMGS
-                    },
-                    {
-                        nombreUsuario: "Feli99",
-                        textoComentario: "¿Alguien sabe si este corrector líquido funciona bien en pieles grasas?",
-                        imagenPerfil: "/img/perfiles/coveruplover.jpg"
-                    }
-                ]
-            },
-            {
-                nombre: "Máscara de pestañas voluminizadora",
-                descripcion: "Máscara de pestañas voluminizadora para unas pestañas más largas y densas.",
-                precio: 21.99,
-                imagen: "/img/productos/mascara.jpg",
-                comentarios: [
-                    {
-                        nombreUsuario: "Vicky31",
-                        textoComentario: "¡Esta máscara de pestañas es increíble! Mis pestañas lucen más largas y voluminosas al instante.",
-                        imagenPerfil: "/img/perfiles/lashlover.jpg"
-                    },
-                    {
-                        nombreUsuario: "JuanPerez",
-                        textoComentario: "¿Alguien ha tenido problemas de grumos con esta máscara de pestañas?",
-                        imagenPerfil: "/img/perfiles/volumeguru.jpg"
-                    }
-                ]
-            },
-            {
-                nombre: "Brillo labial",
-                descripcion: "Brillo labial hidratante con un acabado brillante y sin sensación pegajosa.",
-                precio: 12.99,
-                imagen: "/img/productos/brillo_labial.jpg",
-                comentarios: [
-                    {
-                        nombreUsuario: "RochiJ",
-                        textoComentario: "¡Este brillo labial es perfecto para dar un toque de brillo a cualquier look de maquillaje!",
-                        imagenPerfil: "/img/perfiles/lipglosslover.jpg"
-                    },
-                    {
-                        nombreUsuario: "TomasR",
-                        textoComentario: "¿Alguien más ha notado que este brillo labial tiende a desaparecer rápidamente?",
-                        imagenPerfil: "/img/perfiles/shineonqueen.jpg"
-                    }
-                ]
-            },
-            {
-                nombre: "Iluminador en polvo",
-                descripcion: "Iluminador en polvo para resaltar los puntos altos del rostro y obtener un brillo radiante.",
-                precio: 29.99,
-                imagen: "/img/productos/iluminador.jpg",
-                comentarios: [
-                    {
-                        nombreUsuario: "Trini4566",
-                        textoComentario: "¡Este iluminador es increíble! Me encanta cómo resalta mi piel y le da un brillo natural.",
-                        imagenPerfil: "/img/perfiles/glowgetter.jpg"
-                    },
-                    {
-                        nombreUsuario: "Roco22",
-                        textoComentario: "¿Alguien más ha tenido problemas de brillos excesivos con este iluminador?",
-                        imagenPerfil: "/img/perfiles/highlightqueen.jpg"
-                    }
-                ]
-            },
-            {
-                nombre: "Esmalte de uñas",
-                descripcion: "Esmalte de uñas de larga duración con una amplia gama de colores brillantes y pigmentados.",
-                precio: 9.99,
-                imagen: "/img/productos/esmalte.jpg",
-                comentarios: [
-                    {
-                        nombreUsuario: "Laucha00",
-                        textoComentario: "¡Este esmalte de uñas es fantástico! Los colores son vibrantes y la fórmula dura mucho tiempo sin descascararse.",
-                        imagenPerfil: "/img/perfiles/nailpolishaddict.jpg"
-                    },
-                    {
-                        nombreUsuario: "TomiZ",
-                        textoComentario: "¿Alguien ha probado la durabilidad de estos esmaltes en uñas débiles?",
-                        imagenPerfil: "/img/perfiles/colorfulnails.jpg"
-                    }
-                ]
-            },
-            {nombre: "Paleta de sombras de ojos",
+    productos: [
+        {
+            nombre: "Corrector líquido",
+            descripcion: "Corrector líquido de alta cobertura para ocultar imperfecciones y ojeras.",
+            precio: 17.99,
+            imagen: "/img/productos/corrector.jpg",
+            comentarios: [
+                {
+                    nombreUsuario: "Valen123",
+                    textoComentario: "Este corrector líquido es mi salvavidas. Cubre perfectamente mis ojeras sin verse pesado.",
+                    imagenPerfil: "./public/products/img-cafetera-moulinex.png" //HAY QUE CAMBIAR LA RUTA DE IMGS
+                },
+                {
+                    nombreUsuario: "Feli99",
+                    textoComentario: "¿Alguien sabe si este corrector líquido funciona bien en pieles grasas?",
+                    imagenPerfil: "/img/perfiles/coveruplover.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Máscara de pestañas voluminizadora",
+            descripcion: "Máscara de pestañas voluminizadora para unas pestañas más largas y densas.",
+            precio: 21.99,
+            imagen: "/img/productos/mascara.jpg",
+            comentarios: [
+                {
+                    nombreUsuario: "Vicky31",
+                    textoComentario: "¡Esta máscara de pestañas es increíble! Mis pestañas lucen más largas y voluminosas al instante.",
+                    imagenPerfil: "/img/perfiles/lashlover.jpg"
+                },
+                {
+                    nombreUsuario: "JuanPerez",
+                    textoComentario: "¿Alguien ha tenido problemas de grumos con esta máscara de pestañas?",
+                    imagenPerfil: "/img/perfiles/volumeguru.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Brillo labial",
+            descripcion: "Brillo labial hidratante con un acabado brillante y sin sensación pegajosa.",
+            precio: 12.99,
+            imagen: "/img/productos/brillo_labial.jpg",
+            comentarios: [
+                {
+                    nombreUsuario: "RochiJ",
+                    textoComentario: "¡Este brillo labial es perfecto para dar un toque de brillo a cualquier look de maquillaje!",
+                    imagenPerfil: "/img/perfiles/lipglosslover.jpg"
+                },
+                {
+                    nombreUsuario: "TomasR",
+                    textoComentario: "¿Alguien más ha notado que este brillo labial tiende a desaparecer rápidamente?",
+                    imagenPerfil: "/img/perfiles/shineonqueen.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Iluminador en polvo",
+            descripcion: "Iluminador en polvo para resaltar los puntos altos del rostro y obtener un brillo radiante.",
+            precio: 29.99,
+            imagen: "/img/productos/iluminador.jpg",
+            comentarios: [
+                {
+                    nombreUsuario: "Trini4566",
+                    textoComentario: "¡Este iluminador es increíble! Me encanta cómo resalta mi piel y le da un brillo natural.",
+                    imagenPerfil: "/img/perfiles/glowgetter.jpg"
+                },
+                {
+                    nombreUsuario: "Roco22",
+                    textoComentario: "¿Alguien más ha tenido problemas de brillos excesivos con este iluminador?",
+                    imagenPerfil: "/img/perfiles/highlightqueen.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Esmalte de uñas",
+            descripcion: "Esmalte de uñas de larga duración con una amplia gama de colores brillantes y pigmentados.",
+            precio: 9.99,
+            imagen: "/img/productos/esmalte.jpg",
+            comentarios: [
+                {
+                    nombreUsuario: "Laucha00",
+                    textoComentario: "¡Este esmalte de uñas es fantástico! Los colores son vibrantes y la fórmula dura mucho tiempo sin descascararse.",
+                    imagenPerfil: "/img/perfiles/nailpolishaddict.jpg"
+                },
+                {
+                    nombreUsuario: "TomiZ",
+                    textoComentario: "¿Alguien ha probado la durabilidad de estos esmaltes en uñas débiles?",
+                    imagenPerfil: "/img/perfiles/colorfulnails.jpg"
+                }
+            ]
+        },
+        {
+            nombre: "Paleta de sombras de ojos",
             descripcion: "Paleta de sombras de ojos con una gama de tonos vibrantes y mates.",
             precio: 39.99,
             imagen: "/img/productos/sombras.jpg",
@@ -187,7 +188,7 @@ const products={
                 }
             ]
         }
-            // Agrega más productos de maquillaje aquí...
-        ]
-    };
-    module.exports= products;
+        // Agrega más productos de maquillaje aquí...
+    ]
+};
+module.exports = products;
--- 'ti_progra2'/routes/users.js
+++ 'ti_progra2'/routes/users.js
@@ -8,6 +8,6 @@ const usersController = require('../controllers/usersController');
 router.get('/login', usersController.ingreso);
 router.get('/register', usersController.registro);
 router.get('/profile', usersController.perfil);
-router.get('/profile-edit.html', usersController.edicionPerfil);   // esta bien??? o solo edit?
+router.get('/profile-edit', usersController.edicionPerfil);   // esta bien??? o solo edit? o /profile-edit.html???
 
 module.exports = router;
