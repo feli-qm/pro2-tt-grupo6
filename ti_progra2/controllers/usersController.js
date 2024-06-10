b5158ebbdadf7a0d3e026794e2a53e61bb0c1295
VickyForginieri <vforginieri@udesa.edu.ar>
2024-06-10 17:02:29 -0300

    register

--- ti_progra2/controllers/usersController.js
+++ ti_progra2/controllers/usersController.js
@@ -1,5 +1,6 @@
 //importar info que voy a usar en este modulo
 const db = require('../database/models');
+const bcrypt = require("bcryptjs");
 const op = db.Sequelize.Op;
 const {validationResult} = require("express-validator")
 
@@ -38,24 +39,25 @@ const usersController = {
     let errors = validationResult(req);
     if(errors.isEmpty()){
       //No hay errores, entonces seguimos 
+      return res.send("OK")
     }
     else{
-       res.render('register', {errors: errors.mapped(), old: req.body});
-    let guardar = {
-      email: form.email,
-      nombre: form.usuario,
-      contrasenia: form.contrasena,
-      fechaNacimiento: form.fecha_nacimiento,
-      numeroDocumento: form.nro_documento,
-      foto: form.foto_perfil,
+      return res.render('register', {errors: errors.mapped(), old: req.body});
+      let usuarioCreado = {
+        email: form.email,
+        nombre: form.usuario,
+        contrasenia: bcrypt.hashSync(form.contrasena, 10),
+        fechaNacimiento: form.fecha_nacimiento,
+        numeroDocumento: form.nro_documento,
+        foto: form.foto_perfil,
     }
 
-    db.Usuario.create(form)
+    db.Usuario.create(usuarioCreado)
     .then((resultados) => {
-      return res.redirect("/users")
+      return res.redirect("/")
     }).catch((err) => {
       return console.log(err);
-  })            
+  });         
   }
 }
 }
--- ti_progra2/routes/users.js
+++ ti_progra2/routes/users.js
@@ -3,33 +3,29 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
 
+var db = require('../database/models');
+
+
 //validaciones PREGUNTAR SI VA ACA O EN ROUTES INDEX
 const {body} = require("express-validator");
 let validations = [
     body("email")
-    .notEmpty().withMessage("Debes completar este campo con tu email")
-    .isEmail().withMessage("verifica que este email sea valido")
-    .custom(function(value){
-        db.Usuario.findOne({
-            where: {email : value}, 
-        })
-        .then(function(nombre){
-            if(nombre){
-                throw new Error("El email ingresado no existe") 
-            }
-        })
-}),
+    .notEmpty().withMessage("Debes completar este campo con tu email").bail()
+    .isEmail().withMessage("verifica que este email sea valido").bail()
+   ,
     body("usuario")
     .notEmpty().withMessage("Debes completar este campo con tu nombre de usuario"),
     body("contrasena")
     .notEmpty().withMessage("Debes completar este campo con tu contraseña")
     .isLength({min:4}).withMessage("Debes ingresar un minimo de 4 caracteres"),   
 ]
-router.post("/register", validations, usersController.registro);
 //crear rutas con sus sufijos//
 /* GET users listing. */
 router.get('/login', usersController.ingreso);
 router.get('/register', usersController.registro);
+
+router.post('/login', usersController.ingreso);
+router.post('/register', validations, usersController.store);
 router.get('/profile', usersController.perfil);
 router.get('/edit', usersController.edicionPerfil);
 
